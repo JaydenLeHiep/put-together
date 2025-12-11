37ad4561a8cf06a553752dbcf14e74e81c25f9eb
JaydenLeHiep <98736015+JaydenLeHiep@users.noreply.github.com>
2025-12-11 22:40:06 +0100

    intialize: backend asp.net + frontend react, setup: setup Serilog for backend

---
+++ backend-put-together/Program.cs
@@ -0,0 +1,43 @@
+using backend_put_together;
+using Serilog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Services.AddScoped<WeatherService>();
+builder.Services.AddOpenApi();
+
+builder.Host.UseSerilog();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weather", (WeatherService service, ILogger<Program> logger) =>
+{
+    logger.LogInformation("Weather endpoint called");
+    service.DoSomething();
+    return "OK";
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
