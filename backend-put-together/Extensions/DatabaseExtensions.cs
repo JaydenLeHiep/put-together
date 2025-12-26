6d115bc54c395e481ddc841d17d769cda0f558fb
JaydenLeHiep <98736015+JaydenLeHiep@users.noreply.github.com>
2025-12-26 22:10:32 +0100

    setup: setup db connection + inital setup of minimal structure of project

---
+++ backend-put-together/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,20 @@
+using backend_put_together.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_put_together.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration config)
+    {
+        var conn = Environment.GetEnvironmentVariable("DATABASE_URL")
+                   ?? config.GetConnectionString("DefaultConnection");
+        
+        if(string.IsNullOrWhiteSpace(conn))
+            throw new InvalidOperationException("Could not get connection string");
+
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(conn));
+
+        return services;
+    }
+}
